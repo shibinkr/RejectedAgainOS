9d649309ca286fbd55eaa48eccf6ad37cfa06fe1
Shibin Koikkara Reeny <shibinreeny@gmail.com>
2025-06-04 02:16:35 +0100

    Prints Hello World on screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// #![no_std]             // no standard library
+// #![no_main]            // don't use normal Rust entry point
+
+// use core::panic::PanicInfo;
+
+// /// This function is called on panic.
+// #[panic_handler]
+// fn panic(_info: &PanicInfo) -> ! {
+//     loop {}
+// }
+
+// /// This function is called by the bootloader, like `main()`
+// #[no_mangle]
+// pub extern "C" fn _start() -> ! {
+//     let vga_buffer = 0xb8000 as *mut u8;
+
+//     unsafe {
+//         *vga_buffer.offset(0) = b'H';
+//         *vga_buffer.offset(1) = 0x0f;
+//         *vga_buffer.offset(2) = b'e';
+//         *vga_buffer.offset(3) = 0x0f;
+//         *vga_buffer.offset(4) = b'l';
+//         *vga_buffer.offset(5) = 0x0f;
+//         *vga_buffer.offset(6) = b'l';
+//         *vga_buffer.offset(7) = 0x0f;
+//         *vga_buffer.offset(8) = b'o';
+//         *vga_buffer.offset(9) = 0x0f;
+//     }
+
+//     loop {}
+// }
